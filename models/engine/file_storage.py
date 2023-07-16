f660a6a6644a586fcbc3bb1d5845f0a53631e72f
shield <myshield02@gmail.com>
2023-07-16 13:18:46 +0100

    added class with private attrs and all, new public instance methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """
 This module has a class that serializes instances to a
@@ -50,3 +51,31 @@ class FileStorage:
                     self.new(eval(name)(**obj))
         except FileNotFoundError:
             pass
+
+#!/usr/bin/python3
+"""
+serializes instances to a JSON file and deserializes JSON file to instances
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Handles serialization and deserialization of classes
+    Attr:
+        file_path (str): path to the JSON file
+        objects (dict): storing objects
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets __object with key"""
+        obj_name = obj.__class__.__name__
+        obj_id = obj.id
+        FileStorage.__objects[f"{obj_name}.{obj_id}"] = obj
+
