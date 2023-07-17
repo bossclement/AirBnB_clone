73020bb84929a4aeb454fc0ebe588b9307e89bb5
bossclement <bossclement@gmail.com>
2023-07-17 20:53:12 +0200

    New commit

--- testing.py
+++ testing.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Doc
-"""
+from models import storage
 from models.base_model import BaseModel
 
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
 
-class BaseModel(BaseModel):
-    """Doc
-    """
-
-    def save(self):
-        """Doc
-        """
-        pass
-
-print(BaseModel().save())
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
