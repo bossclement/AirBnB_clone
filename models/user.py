857075823122635b3025b31f6317ab03cf1ef6e2
bossclement <bossclement@gmail.com>
2023-07-22 19:59:42 +0200

    New commit

--- models/user.py
+++ models/user.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """User class model"""
-from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
-
 
 
 class User(BaseModel):
@@ -17,10 +12,7 @@ class User(BaseModel):
         first_name (str): first name string
         last_name (str): last name string
     """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
